68ede7ff7103326e3b80a0f90dcd0f58bf7d0b5a
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-06-08 21:13:44 +0800

    add file updated chess-moves

--- src/app.js
+++ src/app.js
@@ -5,16 +5,53 @@ chessBoard.classList.add('chessboard')
 let cells = []
 
 function createBoard() {
-    let changePattern = false
-    let classToAdd = 'light'
     for (let i = 0; i < 64; i++) {
         cells[i] = document.createElement('div')
-        // cells[i].setAttribute('id', `${i}`)
+        cells[i].setAttribute('id', `${i}`)
         cells[i].classList.add('cell')
-        changePattern = (i % 8 === 0 || i === 0)
-        classToAdd = changePattern ? classToAdd : classToAdd === 'light' ? 'dark' : 'light'
-        cells[i].classList.add(classToAdd)
         chessBoard.appendChild(cells[i])
+    }
+
+    for (let i = 0; i < 8; i++) {
+        if (i % 2 === 0) {
+            cells[i].classList.add('light')
+        } else cells[i].classList.add('dark')
+    }
+
+    for (let i = 8; i < 16; i++) {
+        if (i % 2 === 0) {
+            cells[i].classList.add('dark')
+        } else cells[i].classList.add('light')
+    }
+    for (let i = 16; i < 24; i++) {
+        if (i % 2 === 0) {
+            cells[i].classList.add('light')
+        } else cells[i].classList.add('dark')
+    }
+    for (let i = 24; i < 32; i++) {
+        if (i % 2 === 0) {
+            cells[i].classList.add('dark')
+        } else cells[i].classList.add('light')
+    }
+    for (let i = 32; i < 40; i++) {
+        if (i % 2 === 0) {
+            cells[i].classList.add('light')
+        } else cells[i].classList.add('dark')
+    }
+    for (let i = 40; i < 48; i++) {
+        if (i % 2 === 0) {
+            cells[i].classList.add('dark')
+        } else cells[i].classList.add('light')
+    }
+    for (let i = 48; i < 56; i++) {
+        if (i % 2 === 0) {
+            cells[i].classList.add('light')
+        } else cells[i].classList.add('dark')
+    }
+    for (let i = 56; i < 64; i++) {
+        if (i % 2 === 0) {
+            cells[i].classList.add('dark')
+        } else cells[i].classList.add('light')
     }
 }
 
@@ -23,6 +60,7 @@ createBoard()
 document.body.append(chessBoard)
 
 const board = [[], [], [], [], [], [], [], []]
+
 
 cells.forEach((element, index) => {
     if (index < 8) {
@@ -64,12 +102,12 @@ board[1][7].id = "white-pawn"
 
 board[7][0].id = "black-rook"
 board[7][1].id = "black-knight"
-board[7][2].id = "black-bishop"
+board[4][4].id = "black-bishop"
 board[7][3].id = "black-queen"
 board[7][4].id = "black-king"
 board[7][5].id = "black-bishop"
 board[7][6].id = "black-knight"
-board[7][7].id = "black-rook"
+board[5][7].id = "black-rook"
 
 board[6][0].id = "black-pawn"
 board[6][1].id = "black-pawn"
@@ -81,65 +119,74 @@ board[6][6].id = "black-pawn"
 board[6][7].id = "black-pawn"
 // console.log(board[0][0])
 
+//class black and white for all the pieces
+for (let i = 0; i < 8; i++) {
+    board[0][i].classList.add("white")
+    board[1][i].classList.add("white")
+}
+for (let i = 0; i < 8; i++) {
+    board[6][i].classList.add("black")
+    board[7][i].classList.add("black")
+}
+
 const peicesFunction = (e) => {
     const piece = e.target
     const pieceIndex = Array.from(piece.parentElement.children).indexOf(piece)
     let x, y;
 
+    const xyIndex = ()=>{
     x = pieceIndex % 8
     y = (pieceIndex - x) / 8
-
-    console.log(x, y)
-
+    }
     //Knight
     if (piece.id === "white-knight" || piece.id === "black-knight") {
-        // xyIndex()
+        xyIndex()
         const move = new Chessmoves(y, x)
         move.knight()
-        //Rook
-    } else if (piece.id === "white-rook" || piece.id === "black-rook") {
-        // xyIndex()
+        //Black Rook
+    } else if (piece.id === "black-rook") {
+        xyIndex()
         const move = new Chessmoves(y, x)
-        move.rook()
+        move.blackRook()
         //Pawn
     } else if (piece.id === "white-pawn") {
-        // xyIndex()
+        xyIndex()
         const move = new Chessmoves(y, x)
         move.whitepawn()
-        piece.id = ''
     } else if (piece.id === "black-pawn") {
-        // xyIndex()
+        xyIndex()
         const move = new Chessmoves(y, x)
         move.blackpawn()
-    }
-
-
-
+        //BlackBishop
+    } else if (piece.id === "black-bishop") {
+        xyIndex()
+        const move = new Chessmoves(y, x)
+        move.blackBishop()
+    } 
 }
 
 board.forEach(array => { array.forEach(element => element.addEventListener("click", peicesFunction)) })
 
-
 class Chessmoves {
-    constructor(x, y) {
-        this.x = x
+    constructor(y, x) {
         this.y = y
+        this.x = x
     }
     knight() {
-        // const negativeYtop = this.y - 2 === -1 || this.y - 2 === -2 ? 0 : this.y - 2
-        // const negativeYLeftRight = this.y - 1 === -1 ? 0 : this.y - 1
+        const negativeYtop = this.y - 2 === -1 || this.y - 2 === -2 ? 0 : this.y - 2
+        const negativeYLeftRight = this.y - 1 === -1 ? 0 : this.y - 1
         // const negativeYRight = this.y-1===-1?right1=undefined:this.y-1
 
-        const top1 = board[this.y - 2][this.x - 1]
-        const top2 = board[this.y - 2][this.x + 1]
+        const top1 = board[negativeYtop][this.x - 1]
+        const top2 = board[negativeYtop][this.x + 1]
         // Bottom
         const bottom1 = board[this.y + 2][this.x - 1]
         const bottom2 = board[this.y + 2][this.x + 1]
         // Left
-        const left1 = board[this.y - 1][this.x - 2]
+        const left1 = board[negativeYLeftRight][this.x - 2]
         const left2 = board[this.y + 1][this.x - 2]
         // Right
-        const right1 = board[this.y - 1][this.x + 2]
+        const right1 = board[negativeYLeftRight][this.x + 2]
         const right2 = board[this.y + 1][this.x + 2]
 
         const filterMOves = [top1, top2, bottom1, bottom2, left1, left2, right1, right2]
@@ -149,24 +196,128 @@ class Chessmoves {
 
         knightMoves.forEach(element => element.style.backgroundColor = "blue")
     }
-    rook() {
+    blackRook() {
         //Vertical Move
-        const verticalMove = [board[0][this.y], board[1][this.y], board[2][this.y], board[3][this.y], board[4][this.y], board[5][this.y], board[6][this.y], board[7][this.y]]
-        verticalMove.forEach(element => element.style.backgroundColor = "blue")
+        //Bottom
+        for (let i = this.y+1 ; i < 8; i++) {
+            const capture = (e)=>{
+                const targetBox = e.target
+                targetBox.id = "black-rook"
+                targetBox.classList.remove("white")
+                targetBox.classList.add("black")
+    
+                board[this.y][this.x].id = ""
+                board[this.y][this.x].classList.remove("black")
+            }
+            if(board[i][this.x].classList.contains("black")){
+                break
+            }else if(board[i][this.x].classList.contains("white")){
+                // board[i][this.x].style.backgroundColor = "yellow"
+                board[i][this.x].addEventListener("click",capture)
+                break
+            }else{
+                board[i][this.x].addEventListener("click",capture)
+            }
+        }
+        //Top
+        for (let i = this.y-1 ; i > -1; i--) {
+            const capture = (e)=>{
+                const targetBox = e.target
+                targetBox.id = "black-rook"
+                targetBox.classList.remove("white")
+                targetBox.classList.add("black")
+    
+                board[this.y][this.x].id = ""
+                board[this.y][this.x].classList.remove("black")
+            }
+            if(board[i][this.x].classList.contains("black")){
+                break
+            }else if(board[i][this.x].classList.contains("white")){
+                // board[i][this.x].style.backgroundColor = "yellow"
+                board[i][this.x].addEventListener("click",capture)
+                break
+            }else{
+                board[i][this.x].addEventListener("click",capture)
+            }
+        }
         //Horizontal Move
-        board[this.x].forEach(element => element.style.backgroundColor = "blue")
+        //Left
+        for (let i = this.x-1 ; i > -1; i--) {
+            const capture = (e)=>{
+                const targetBox = e.target
+                targetBox.id = "black-rook"
+                targetBox.classList.remove("white")
+                targetBox.classList.add("black")
+    
+                board[this.y][this.x].id = ""
+                board[this.y][this.x].classList.remove("black")
+            }
+            if(board[this.y][i].classList.contains("black")){
+                break
+            }else if(board[this.y][i].classList.contains("white")){
+                // board[this.y][i].style.backgroundColor = "yellow"
+                board[this.y][i].addEventListener("click",capture)
+                break
+            }else{
+                board[this.y][i].addEventListener("click",capture)
+            }
+        }
+        //Right
+        for (let i = this.x+1 ; i < 8; i++) {
+            const capture = (e)=>{
+                const targetBox = e.target
+                targetBox.id = "black-rook"
+                targetBox.classList.remove("white")
+                targetBox.classList.add("black")
+    
+                board[this.y][this.x].id = ""
+                board[this.y][this.x].classList.remove("black")
+            }
+            if(board[this.y][i].classList.contains("black")){
+                break
+            }else if(board[this.y][i].classList.contains("white")){
+                board[this.y][i].addEventListener("click",capture)
+                break
+            }else{
+                board[this.y][i].addEventListener("click",capture)
+            }
+        }
+    }
+    blackBishop() {
+        
+        let addTopRight = 1
+        for (let i = this.y-1 ; i > -1; i--) {
+            const capture = (e)=>{
+                const targetBox = e.target
+                targetBox.id = "black-bishop"
+                targetBox.classList.remove("white")
+                targetBox.classList.add("black")
+    
+                board[this.y][this.x].id = ""
+                board[this.y][this.x].classList.remove("black")
+            }
+            if(board[i][this.x+addTopRight].classList.contains("black")){
+                break
+            }else if(board[i][this.x+addTopRight].classList.contains("white")){
+                board[i][this.x+addTopRight].style.backgroundColor = "blue"
+                board[i][this.x+addTopRight].addEventListener("click",capture)
+                break
+            }else{
+                board[i][this.x+addTopRight].style.backgroundColor = "yellow"
+                board[i][this.x+addTopRight].addEventListener("click",capture)
+            }
+            addTopRight += 1
+        }
+    
     }
     whitepawn() {
         //Vertical Move
-        const moveForward = board[this.x + 1][this.y]
+        const moveForward = board[this.y + 1][this.x]
         moveForward.style.backgroundColor = "blue"
-        // moveForward.addEventListener('click', function () {
-        //     moveForward.id = 'white-pawn'
-        // })
     }
     blackpawn() {
         //Vertical Move
-        const moveForward = board[this.x - 1][this.y]
+        const moveForward = board[this.y - 1][this.x]
         moveForward.style.backgroundColor = "blue"
     }
 }
